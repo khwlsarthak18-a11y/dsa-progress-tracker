8c275a4b2154015ea54eca7ffea875add11a8506
first <khwlsarthak18@gmail.com>
2026-05-31 15:41:51 +0530

    rename html

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 
@@ -12,13 +13,84 @@ app.get("/", (req, res) => {
 app.get("/leetcode/:username", async (req, res) => {
   const username = req.params.username;
 
-  res.json({
-    username: username,
-    solved: 123,
-    easy: 50,
-    medium: 60,
-    hard: 13
-  });
+  const query = `
+    query userProfile($username: String!) {
+      matchedUser(username: $username) {
+      profile {
+  ranking
+}
+        submitStats {
+          acSubmissionNum {
+            difficulty
+            count
+          }
+        }
+    
+      }
+    }
+  `;
+
+  try {
+    const response = await axios.post(
+      "https://leetcode.com/graphql",
+      {
+        query,
+        variables: { username }
+      },
+      {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    );
+
+    const stats =
+      response.data.data.matchedUser.submitStats.acSubmissionNum;
+     
+    const ranking =
+  response.data.data.matchedUser.profile.ranking;
+    const easy = stats.find(x => x.difficulty === "Easy")?.count || 0;
+    const medium = stats.find(x => x.difficulty === "Medium")?.count || 0;
+    const hard = stats.find(x => x.difficulty === "Hard")?.count || 0;
+    const solved = easy + medium + hard;
+    let level = "Beginner";
+
+if (solved >= 300) level = "Advanced";
+else if (solved >= 100) level = "Intermediate";
+let nextTopic = "Arrays";
+
+if (solved < 50) nextTopic = "Sliding Window";
+else if (solved < 100) nextTopic = "Binary Search";
+else if (solved < 200) nextTopic = "Trees";
+else nextTopic = "Dynamic Programming";
+
+   res.json({
+  username,
+  solved,
+  easy,
+  medium,
+  hard,
+  ranking,
+  level,
+  nextTopic,
+  roadmap: [
+    "Arrays",
+    "Hashing",
+    "Two Pointers",
+    "Sliding Window",
+    "Binary Search",
+    "Trees",
+    "Graphs",
+    "Dynamic Programming"
+  ]
+});
+  } catch (err) {
+    console.log(err.response?.data || err.message);;
+
+    res.status(500).json({
+      error: err.message
+    });
+  }
 });
 
 app.listen(5000, () => {
